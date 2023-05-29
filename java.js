ba6716fdf2d69d1ad6162f9114d3d28e456f0b7e
Rambhai567 <131847922+Rambhai567@users.noreply.github.com>
2023-05-29 12:43:20 +0530

    Add files via upload

--- java.js
+++ java.js
@@ -3,3 +3,15 @@ hamburger.onclick = function() {
   navBar = document.querySelector(".nav-bar");
   navBar.classList.toggle("activa");
 };
+
+
+
+
+
+// preloader 
+
+var loader = document.getElementById("preloader");
+
+window.addEventListener("load" , function () {
+  loader.style.display= "none";
+});
