24a553fc58f80c4936bef6f126b5ab3272ad7d73
Ram <131847922+Rambhai567@users.noreply.github.com>
2023-07-30 19:27:46 +0530

    Add files via upload

--- java.js
+++ java.js
@@ -19,5 +19,10 @@ window.addEventListener("load" , function () {
 
 
 
-
-
+// dark mode
+const body = document.querySelector('body');
+const toggle = document.getElementById('toggle');
+toggle.onclick = function(){
+    toggle.classList.toggle('active')
+    body.classList.toggle('active')
+}
